a70a2754667b2b5238dee515105ade52d11776a0
Santiago Paiz <santiago.paiz@gmail.com>
2018-03-19 11:36:58 -0600

    addUser mejorada

--- src/GUI/AddUser.java
+++ src/GUI/AddUser.java
@@ -1,4 +1,13 @@
 package GUI;
 
+import javax.swing.*;
+
 public class AddUser {
+    private JLabel image;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTable table1;
+    private JButton addButton;
 }
